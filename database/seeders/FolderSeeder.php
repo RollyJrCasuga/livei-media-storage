2d9ce97c51efd69832f63ed4e755bbf9b810a923
Rolly Jr. Casuga <arjong@Rollys-MBP.localdomain>
2021-02-23 15:07:31 +0800

    update seeder - remove folder seeder

--- database/seeders/FolderSeeder.php
+++ database/seeders/FolderSeeder.php
@@ -18,40 +18,40 @@ class FolderSeeder extends Seeder
     {
         FileStorage::deleteDirectory(public_path('/media/'));
 
-        $folder_path = '/media/youtube/Documents/';
-        Folder::create([
-            'id' => 1,
-            'parent_id' => null,
-            'name' => 'Documents',
-            'folder_path' => $folder_path
-        ]);
-        $create_path = public_path($folder_path);
-        if(!FileStorage::isDirectory($create_path)){
-            FileStorage::makeDirectory($create_path, 0777, true, true);
-        }
+        // $folder_path = '/media/youtube/Documents/';
+        // Folder::create([
+        //     'id' => 1,
+        //     'parent_id' => null,
+        //     'name' => 'Documents',
+        //     'folder_path' => $folder_path
+        // ]);
+        // $create_path = public_path($folder_path);
+        // if(!FileStorage::isDirectory($create_path)){
+        //     FileStorage::makeDirectory($create_path, 0777, true, true);
+        // }
 
-        $folder_path = '/media/youtube/Documents/Beach/';
-        Folder::create([
-            'id' => 2,
-            'parent_id' => 1,
-            'name' => 'Beach',
-            'folder_path' => $folder_path
-        ]);
-        $create_path = public_path($folder_path);
-        if(!FileStorage::isDirectory($create_path)){
-            FileStorage::makeDirectory($create_path, 0777, true, true);
-        }
+        // $folder_path = '/media/youtube/Documents/Beach/';
+        // Folder::create([
+        //     'id' => 2,
+        //     'parent_id' => 1,
+        //     'name' => 'Beach',
+        //     'folder_path' => $folder_path
+        // ]);
+        // $create_path = public_path($folder_path);
+        // if(!FileStorage::isDirectory($create_path)){
+        //     FileStorage::makeDirectory($create_path, 0777, true, true);
+        // }
 
-        $folder_path = '/media/youtube/Folder/';
-        Folder::create([
-            'id' => 3,
-            'parent_id' => null,
-            'name' => 'Folder',
-            'folder_path' => $folder_path
-        ]);
-        $create_path = public_path($folder_path);
-        if(!FileStorage::isDirectory($create_path)){
-            FileStorage::makeDirectory($create_path, 0777, true, true);
-        }
+        // $folder_path = '/media/youtube/Folder/';
+        // Folder::create([
+        //     'id' => 3,
+        //     'parent_id' => null,
+        //     'name' => 'Folder',
+        //     'folder_path' => $folder_path
+        // ]);
+        // $create_path = public_path($folder_path);
+        // if(!FileStorage::isDirectory($create_path)){
+        //     FileStorage::makeDirectory($create_path, 0777, true, true);
+        // }
     }
 }
